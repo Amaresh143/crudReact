2c4f560744b06c458c368b63a3bdd94d87359f8e
Amaresh143 <kumaramaresh.r@gmail.com>
2024-05-06 17:17:19 +0530

    counter with condition

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+
+import { useState } from 'react'
 
 const Home = () => {
+  const [count, setCount] = useState(0)
+
+  const inc = () => {
+    setCount (count+1)
+     
+    
+  }
+  
+  const dec = () => {
+    if (count === 0) {
+      setCount(0)
+      console.log("secomnf")
+    } else {
+      setCount (count-1)
+    }
+
+ 
+    
+  }
+  const reset = () => {
+    setCount(0)
+    console.log("first")
+     
+    
+  }
+  
+
+
   return (
-    <div>Home</div>
+    <div>
+      <p >{count}</p>
+      <button onClick={inc}>Incrment</button>
+      <button onClick={dec}>Decrement</button>
+      
+      <button onClick={reset}>reset</button>
+    </div>
   )
 }
 
